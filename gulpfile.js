d55ad6073670bc2cebfc0f3f9caef5a3b06ecda2
Katlyn Whittenburg <Katlyn@Katlyns-MacBook-Pro.local>
2015-05-31 13:26:38 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  sass = require('gulp-sass');
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+//run sass
+gulp.task('sass', function () {
+    return sass('styles/main.scss')
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+   })
+    .pipe(gulp.dest('styles/css/'));
+});
+
+//run sass again when file is updated
+gulp.task('watch', function(){
+  gulp.watch('styles/scss/*.scss', ['sass']);
+});
+
+//reload server when sass file updates
+gulp.task('serve', function() {
+    var server = gls.new('server.js');
+    server.start();
+    gulp.watch(['styles/scss/*.scss'], server.notify);
+    gulp.watch('server.js', server.start);
+});
+
+//initial run
+gulp.task('default', ['serve', 'sass', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch('styles/main.scss', ['sass']);
+})
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
