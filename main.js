354e564c18cadcabfd29ba0748ddb8625280abf4
Katlyn Whittenburg <katlynwhittenburg@gmail.com>
2015-06-06 23:16:22 -0400

    placed pieces of letters

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 $('button').click(function(){
-  $('.k1').css('transform', 'translate3d(550px, -300px, 0)');
-  $('.k2').css('transform', 'translate3d(350px, -400px, 0)');
-  $('.lineA').css('transform', 'translate3d(350px, -400px, 0)');
-  $('.a2').css('transform', 'translate3d(550px, 500px, 0)');
-  $('.t1').css('transform', 'translate3d(150px, 400px, 0)');
-  $('.t2').css('transform', 'translate3d(250px, -100px, 0)');
-  $('.t3').css('transform', 'translate3d(350px, -2250px, 0)');
-  $('.l1').css('transform', 'translate3d(350px, 240px, 0)');
-  $('.y1').css('transform', 'translate3d(350px, -520px, 0)');
-  $('.y2').css('transform', 'translate3d(350px, -400px, 0)');
-  $('.n1').css('transform', 'translate3d(350px, -400px, 0)');
-  $('.n2').css('transform', 'translate3d(350px, -400px, 0)');
+  $('.k1').css('transform', 'translate3d(310px, -90px, 0)');
+  $('.k2').css('transform', 'translate3d(-197px, -470px, 0)');
+  $('.lineA').css('transform', 'translate3d(375px, -280px, 0)');
+  $('.a2').css('transform', 'translate3d(510px, -480px, 0)');
+  $('.t1').css('transform', 'translate3d(-450px, -400px, 0)');
+  $('.t2').css('transform', 'translate3d(-550px, -300px, 0)');
+  $('.t3').css('transform', 'translate3d(-300px, -250px, 0)');
+  $('.l1').css('transform', 'translate3d(-502px, -190px, 0)');
+  $('.y1').css('transform', 'translate3d(184px, -390px, 0)');
+  $('.y2').css('transform', 'translate3d(400px, -200px, 0)');
+  $('.n1').css('transform', 'translate3d(-440px, -70px, 0)');
+  $('.n2').css('transform', 'translate3d(-200px, -17px, 0)');
 });
