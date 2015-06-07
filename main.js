acfe8bde734b5fc7119353740f79314ea784640d
Katlyn Whittenburg <katlynwhittenburg@gmail.com>
2015-06-06 22:14:06 -0400

    adding animation

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-main.js
+$('button').click(function(){
+  $('.k1').css('transform', 'translate3d(550px, -300px, 0)');
+  $('.k2').css('transform', 'translate3d(350px, -400px, 0)');
+  $('.lineA').css('transform', 'translate3d(350px, -400px, 0)');
+  $('.a2').css('transform', 'translate3d(550px, 500px, 0)');
+  $('.t1').css('transform', 'translate3d(150px, 400px, 0)');
+  $('.t2').css('transform', 'translate3d(250px, -100px, 0)');
+  $('.t3').css('transform', 'translate3d(350px, -2250px, 0)');
+  $('.l1').css('transform', 'translate3d(350px, 240px, 0)');
+  $('.y1').css('transform', 'translate3d(350px, -520px, 0)');
+  $('.y2').css('transform', 'translate3d(350px, -400px, 0)');
+  $('.n1').css('transform', 'translate3d(350px, -400px, 0)');
+  $('.n2').css('transform', 'translate3d(350px, -400px, 0)');
+});
